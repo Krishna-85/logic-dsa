6ded44cc85dcdcbf0b982ff3f3afdc21f5806615
krishna-85 <vaibhavchauhan658@gmail.com>
2025-01-23 11:57:23 +0530

    solved 1 question

--- Recursion/script.js
+++ Recursion/script.js
@@ -1,1 +1,160 @@
 //LIFO :- Last in first out.
+
+ 
+
+//+++++++++++++++++ Print 5 To 1 +++++++++++++++++++++++++++++++++++++++++++++++
+
+// let n = Number(prompt("Enter a number"))
+// function printHello(n){
+//     if(n==0) return
+//     console.log(n)
+//     printHello(n-1)
+    
+// }
+// printHello(n)
+
+// Print 1 To 5 Count,
+
+//+++++++++++++++++++++++++ Print The Sum +++++++++++++++++++++++++++++++++++++++++++++++
+// jo kuch bhi hold par rehta hai wo tab chlta hai jab function pop/backtrack hone par, this is called backTracking.
+
+// let n = Number(prompt("Enter a number"))
+// function sum(n){
+//     if(n==1) return 1
+//        return n + sum(n-1)
+//     }
+//     console.log("sum " + sum(n))
+    
+//+++++++++++++++++++++++ Factorial +++++++++++++++++++++++++++++++++++++++++++++++++
+
+// let n = Number(prompt("Enter a number"))
+// function fact(n){
+//     if(n==1) return 1
+//        return n * fact(n-1)
+//     }
+//     console.log("fact " + fact(n))
+
+    
+
+//+++++++++++++++++++++++++++ Sum of Digit +++++++++++++++++++++++++++++++++++++++++++++
+
+// let n = Number(prompt("Enter a number"))
+// function sumofDig(n){
+//     if(n==0) return n
+//        return (n%10) + sumofDig(Math.floor(n/10))
+//     }
+//   console.log(sumofDig(n))
+
+//+++++++++++++++++++++++++++ Sum of Digit +++++++++++++++++++++++++++++++++++++++++++++
+
+// let n = Number(prompt("Enter a number"))
+// let rev = 0
+// function sumofDig(n, rev){
+//     if(n==0) return n
+//        return (rev*10) + sumofDig(Math.floor(n/10), rev)
+//     }
+//   console.log(sumofDig(n, rev))
+
+//+++++++++++++++++++++++++++ Reverse of Digit +++++++++++++++++++++++++++++++++++++++++++++
+
+// let n = Number(prompt("Enter a number"))
+// let rev = 0
+// function sumofDig(n, rev){
+//     if(n==0) return rev
+//     let rem = n%10
+//         rev =  (rev*10) + rem
+//        return sumofDig(Math.floor(n/10), rev)
+//     }
+//   console.log(sumofDig(n, rev))
+
+//+++++++++++++++++++++++++++ Recursion calls +++++++++++++++++++++++++++++++++++++++++++++
+
+// let n = Number(prompt("Enter a number"))
+// function fibo(n){
+//     if(n==0 || n==1 ) return n
+//     return fibo(n-1) + fibo(n-2)
+ 
+// }
+// console.log(fibo(n))
+
+//+++++++++++++++++++++++++++ Print Fibonacci sequence +++++++++++++++++++++++++++++++++++++++++++++
+
+// let n = Number(prompt("Enter a number"))
+
+// let first = 0, second =1
+// console.log(first)
+// console.log(second)
+// for(let i=1; i<=n-2; i++){
+//     let third = first + second
+//     console.log(third)
+//     first = second
+//     second = third
+// }
+
+//+++++++++++++++++++++++++++ GCD with recurision +++++++++++++++++++++++++++++++++++++++++++++
+
+// let a = Number(prompt("enter a number"))
+// let b = Number(prompt("enter a number"))
+// let c = Math.min(a,b)
+// function gcd(a,b,c){
+//     if(c==1) return 1
+//     if(a%c==0 && b%c==0){
+//         return c
+//     }
+//     return gcd(a,b,c-1)
+// }
+// console.log(gcd(a,b,c))
+
+
+//+++++++++++++++++++++++++++ GCD with recurision, second method +++++++++++++++++++++++++++++++++++++++++++++
+
+// let a = Number(prompt("enter a number"))
+// let b = Number(prompt("enter a number"))
+
+// console.log(gcd(a,b))
+// function gcd(a,b){
+//     if(a==b) return a
+//     if(a>b) return gcd(a-b ,b)
+//         return gcd(a, b-a)
+// }
+
+//=====================================================================================
+
+
+//  let n = Number(prompt("enter a number"))
+
+//  let factor = (n,i)=>{
+//     if(i>n/2){
+//         console.log(n)
+//         return 
+//     }
+//     if(n%i ==0){
+//         console.log(i)
+//     }
+//     factor(n, i+1)
+//  }
+// factor(n,1)
+
+//==================================================\\
+
+// let n = Number(prompt("enter a number"))
+// let arr = []
+// for(let i=0; i<=Math.floor(Math.sqrt(n)); i++){
+//     if(n%i==0){
+//         if(i==n/i){
+//             arr.push(i)
+//         }else{
+//             arr.push(i)
+
+//             arr.push(n/i)
+
+//         }
+//     }
+// }
+// arr.sort((a,b)=> (a-b))
+// console.log(arr)
+
+//Solved but consuming extra space.
+//==================================================\\
+
+
