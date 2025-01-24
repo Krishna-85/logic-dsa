68eb67b1a0cda21548dcaf659a36475897b49beb
krishna-85 <vaibhavchauhan658@gmail.com>
2025-01-24 14:27:34 +0530

    prime number second method

--- Recursion/script.js
+++ Recursion/script.js
@@ -187,7 +187,19 @@
 
 // agar koi number 2 se leke apne square root tak devide nhi ho paya to iska mtlb wo aage bhi kabhi devide nhi ho payega.
 
-//================================= Prime Number Second Method ======================================
+//koii sa bhi odd number even number se nhi jata hai.
+//================================= Prime(odd) Number Second Method ======================================
 
-  let n = Number(prompt("enter a number"))
-  
+let n = Number(prompt("enter a number"))
+isPrime()
+if(isPrime(n)) console.log("prime Number")
+      else console.log("not prime number")
+function isPrime(n){
+  if(n<=1) return false
+  if(n==2) return true
+  if(n%2==0) return false
+  for(let i=3; i<=Math.floor(Math.sqrt(n)); i+=2){
+    if(n%i==0) return false
+  }
+  return true
+}
