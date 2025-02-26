2927e8d8c27150bb4bdf92de41f696dcb415e767
krishna-85 <vaibhavchauhan658@gmail.com>
2025-02-26 14:54:48 +0530

    added 2 question more

--- Basic logic Q/script.js
+++ Basic logic Q/script.js
@@ -1048,3 +1048,20 @@
 
 //====================================== Bubble Short ==========================================================
 
+
+// let arr = [15,27,41,85,95,100,102]
+
+
+// for(let i=0; i < arr.length-1; i++){
+    
+//     for(let j=0; j<arr.length-1; j++){
+        
+//         if(arr[j]>arr[j+1]){
+//             let temp = arr[j]
+//             arr[j] = arr[j+1]
+//             arr[j+1] = temp
+//         }
+//     }
+// }
+
+// console.log(arr)
